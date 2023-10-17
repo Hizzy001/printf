ed6f46b058212a5458836998de63b6c0c8f59e1f
Hizzy001 <hisrealh@gmail.com>
2023-10-16 21:53:49 -0700

    printf

---
+++ get_precision.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_precision - Determine the precision for formatting
+ * @format: The formatted string in which to print the arguments
+ * @i: A pointer to the current position in the format string
+ * @list: List of arguments
+ *
+ * Return: The calculated precision value
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	int curr_i = *i + 1;
+	int precision = -1;
+
+	if (format[curr_i] != '.')
+	return (precision);
+
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+	if (is_digit(format[curr_i]))
+	{
+	precision *= 10;
+	precision += format[curr_i] - '0';
+	}
+	else if (format[curr_i] == '*')
+	{
+	curr_i++;
+	precision = va_arg(list, int);
+	break;
+	}
+	else
+	{
+	break;
+	}
+	}
+
+	*i = curr_i - 1;
+	return (precision);
+}
